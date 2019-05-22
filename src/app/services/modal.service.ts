d42d73ee05ef272965301f4d2609cabab2d9b67d
ezaychik <ezaychik@gmail.com>
2019-05-22 16:13:24 -0500

    removed unneeded const and promise handling

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -7,15 +7,10 @@ export class ModalService {
   constructor(private modal: Modal) { }
 
   openModal(message: string) {
-    const dialogRef = this.modal.alert()
+    this.modal.alert()
       .size('sm')
       .body(message)
       .open();
-
-    dialogRef.result
-      .then(
-        result => console.log(result),
-        error => console.log(error));
   }
 }
 
